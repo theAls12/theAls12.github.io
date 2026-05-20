4c2668724ccb4e6ade62b646c80bc70ffe8327b3
Als12 <alexlorente01@gmail.com>
2026-05-21 01:44:53 +0200

    Cambios

--- assets/script.js
+++
@@ -1,2 +0,0 @@
-const observer = new IntersectionObserver((entries)=>{entries.forEach((entry)=>{if(entry.isIntersecting){entry.target.classList.add('visible')}})},{threshold:0.12});
-document.querySelectorAll('.reveal').forEach((el)=>observer.observe(el));
